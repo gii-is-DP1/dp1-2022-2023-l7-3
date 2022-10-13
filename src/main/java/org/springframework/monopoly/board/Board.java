0ab14fbfecbd81b0f98fa23fbf4e618516285b0f
Miguel-Angel-Rolgan-Garcia <migrolgar2@alum.us.es>
2022-10-13 16:52:19 +0200

    Added board entity Fixes #58

---
+++ src/main/java/org/springframework/monopoly/board/Board.java
@@ -0,0 +1,10 @@
+package org.springframework.monopoly.board;
+
+import javax.persistence.Entity;
+
+import org.springframework.monopoly.model.BaseEntity;
+
+@Entity
+public class Board extends BaseEntity {
+
+}
