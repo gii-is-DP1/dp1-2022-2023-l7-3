c22743e26b57351774091e13b48a93cd7a164c90
ursblue <ugsugs333@gmail.com>
2022-10-27 16:34:51 +0200

    Repositorio y Servicio de propiedad

---
+++ src/main/java/org/springframework/monopoly/property/PropertyRepository.java
@@ -0,0 +1,8 @@
+package org.springframework.monopoly.property;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface PropertyRepository extends  CrudRepository<Property, Integer>{
+	
+}
