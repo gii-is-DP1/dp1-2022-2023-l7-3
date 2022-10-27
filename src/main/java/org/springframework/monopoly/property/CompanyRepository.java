6c79282923dab6845c4f0fb364dc74267f0f23f5
juamornav3 <juamornav3@alum.us.es>
2022-10-27 16:49:45 +0200

    Servicios y repositorios de calle y compañia

---
+++ src/main/java/org/springframework/monopoly/property/CompanyRepository.java
@@ -0,0 +1,7 @@
+package org.springframework.monopoly.property;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CompanyRepository extends  CrudRepository<Company, Integer>{
+	
+}
