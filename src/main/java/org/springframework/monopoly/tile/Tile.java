67ffd62686351c5fd5e9c365b0c4a6aeb36d0dce
juamornav3 <juamornav3@alum.us.es>
2022-11-08 17:42:30 +0100

    Arreglada relación entre casilla y juego eliminando la relación juego propiedad

--- src/main/java/org/springframework/monopoly/tile/Tile.java
+++ src/main/java/org/springframework/monopoly/tile/Tile.java
@@ -1,12 +1,19 @@
 package org.springframework.monopoly.tile;
 
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.MappedSuperclass;
 
+import org.springframework.monopoly.game.Game;
 import org.springframework.monopoly.model.BaseEntity;
 
 
 @MappedSuperclass
 public class Tile extends BaseEntity {
+	
+	@ManyToOne
+	@JoinColumn(name = "game_id")
+	private Game game;
 
 
 }
