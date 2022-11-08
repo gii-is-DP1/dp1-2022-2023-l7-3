2fc65b508fbeae124926250fd3ee6011a2c762fd
juamornav3 <juamornav3@alum.us.es>
2022-11-08 13:30:37 +0100

    Serivcio comprar propiedad

--- src/main/java/org/springframework/monopoly/property/PropertyRepository.java
+++ src/main/java/org/springframework/monopoly/property/PropertyRepository.java
@@ -1,8 +1,15 @@
 package org.springframework.monopoly.property;
 
+
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 
+
+
 public interface PropertyRepository extends  CrudRepository<Property, Integer>{
+	
+	Optional<Property>  findById(Integer id);
 	
 }
