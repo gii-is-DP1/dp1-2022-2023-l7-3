817072c55eb213e7cfffa43dcce8db4edbbba7f1
marmarsol4 <marmarsol4@alum.us.es>
2022-11-09 11:13:29 +0100

    Adicion de servicios en cardService, nuevos metodos en CardRepository

--- src/main/java/org/springframework/monopoly/card/CardController.java
+++ src/main/java/org/springframework/monopoly/card/CardController.java
@@ -2,14 +2,16 @@ package org.springframework.monopoly.card;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 
 public class CardController {
 	
-	@Autowired
 	private CardService cardService;
 
+	@Autowired
+	public CardController (CardService cardService) {
+		this.cardService = cardService;
+	}
+	
 }
