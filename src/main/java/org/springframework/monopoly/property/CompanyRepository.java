4f0874b1993a121b30edcd6db233ce34aaa44883
ursgarsan <ugsugs333@gmail.com>
2022-11-10 13:04:59 +0100

    Servicios hechos falta de tirada hipoteca subasta

--- src/main/java/org/springframework/monopoly/property/CompanyRepository.java
+++ src/main/java/org/springframework/monopoly/property/CompanyRepository.java
@@ -1,10 +1,16 @@
 package org.springframework.monopoly.property;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface CompanyRepository extends  CrudRepository<Company, Integer>{
 	
 	boolean findIsCompanyById(Integer id);
 	Company findCompanyById(Integer id);
-	
+
+	@Query("SELECT c FROM Company c WHERE c.owner = :idOwner")
+	List<Company> findByOwner(@Param("idOwner")Integer id);	
 }
