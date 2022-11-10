3fb9b45f2f7b5d9c809103248b79a231f04eab4d
ursgarsan <ugsugs333@gmail.com>
2022-11-10 13:07:00 +0100

    Objeto auxiliar para construir

---
+++ src/main/java/org/springframework/monopoly/property/AuxBuildings.java
@@ -0,0 +1,60 @@
+package org.springframework.monopoly.property;
+
+public class AuxBuildings {
+
+    //poner lista nombres lista cantidades
+    
+    private Boolean hotel;
+    private Integer name1;
+    private Integer name2;
+    private Integer name3;
+    private Integer cant1;
+    private Integer cant2;
+    private Integer cant3;
+
+    public Boolean getHotel() {
+        return hotel;
+    }
+    public void setHotel(Boolean hotel) {
+        this.hotel = hotel;
+    }
+    public Integer getName1() {
+        return name1;
+    }
+    public void setName1(Integer name1) {
+        this.name1 = name1;
+    }
+    public Integer getName2() {
+        return name2;
+    }
+    public void setName2(Integer name2) {
+        this.name2 = name2;
+    }
+    public Integer getName3() {
+        return name3;
+    }
+    public void setName3(Integer name3) {
+        this.name3 = name3;
+    }
+    public Integer getCant1() {
+        return cant1;
+    }
+    public void setCant1(Integer cant1) {
+        this.cant1 = cant1;
+    }
+    public Integer getCant2() {
+        return cant2;
+    }
+    public void setCant2(Integer cant2) {
+        this.cant2 = cant2;
+    }
+    public Integer getCant3() {
+        return cant3;
+    }
+    public void setCant3(Integer cant3) {
+        this.cant3 = cant3;
+    }
+
+ 
+    
+}
