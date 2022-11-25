468942525a883ba9c6cc63485c195e19686128b2
Olegario Morato Navarro <olemornav@alum.us.es>
2022-11-25 09:44:43 +0100

    Cambios en servicios de casillas para que se adapten a la situación del modelo actual

--- src/main/java/org/springframework/monopoly/tile/GenericRepository.java
+++ src/main/java/org/springframework/monopoly/tile/GenericRepository.java
@@ -1,9 +1,17 @@
 package org.springframework.monopoly.tile;
 
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface GenericRepository extends CrudRepository<Generic, Integer> {
 	
-	List<Generic> findAll();
+	@Query("SELECT g FROM Generic g WHERE g.game.id = :gameId")
+	List<Generic> findAll(@Param("gameId") Integer gameId);
+	
+	@Query("SELECT g FROM Generic g WHERE g.game.id = :gameId AND g.id = id")
+	Optional<Generic> findById(@Param("gameId") Integer gameId, @Param("id") Integer id);
 }
--- src/main/java/org/springframework/monopoly/tile/TaxesRepository.java
+++ src/main/java/org/springframework/monopoly/tile/TaxesRepository.java
@@ -1,10 +1,17 @@
 package org.springframework.monopoly.tile;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface TaxesRepository extends CrudRepository<Taxes, Integer>{
 	
-	List<Taxes> findAll();
+	@Query("SELECT t FROM Taxes t WHERE t.game.id = :gameId")
+	List<Taxes> findAll(@Param("gameId") Integer gameId);
+	
+	@Query("SELECT t FROM Taxes t WHERE t.game.id = :gameId AND t.id = id")
+	Optional<Taxes> findById(@Param("gameId") Integer gameId, @Param("id") Integer id);
 }
