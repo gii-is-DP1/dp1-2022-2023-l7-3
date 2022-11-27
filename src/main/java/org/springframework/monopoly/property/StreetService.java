8d2d28778996cef715d6ec45341477052cfc8ab2
Miguel-Angel-Rolgan-Garcia <migrolgar2@alum.us.es>
2022-11-27 18:46:13 +0100

    Added query to find colors of a players' complete streets

--- src/main/java/org/springframework/monopoly/property/StreetService.java
+++ src/main/java/org/springframework/monopoly/property/StreetService.java
@@ -7,6 +7,7 @@ import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
+import org.springframework.monopoly.player.Player;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -38,6 +39,12 @@ public class StreetService {
 		}
 		
 		return new ArrayList<Color>(colors);
+	}
+	
+	// New
+	@Transactional
+	public List<Color> findPlayerColors(Player player) {
+		return streetRepository.findPlayerColors(player.getGame().getId(), player.getId());
 	}
 
 }
